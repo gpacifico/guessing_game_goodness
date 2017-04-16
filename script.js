d7dd1213aea28eb36384257a70fe66e9aac66bd1
Gwenever Pacifico <gwenever.pacifico@gmail.com>
2017-04-16 00:46:17 -0700

    added images and game area and fixed the js for maps and game

--- script.js
+++ script.js
@@ -1,23 +1,25 @@
-// This example requires the Places library. Include the libraries=places
-// parameter when you first load the API. For example:
-// <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=places">
+//THE MAP
 
 var map;
 var infowindow;
 
 function initMap() {
-    var pyrmont = {lat: -33.867, lng: 151.195};
+    // var pyrmont = {lat: -33.867, lng: 151.195};
+    var firestation = {lat: 40.719560, lng: -74.006626};
+
 
     map = new google.maps.Map(document.getElementById('map'), {
-        center: pyrmont,
-        zoom: 15
+        // center: pyrmont,
+        center: firestation,
+        zoom: 14
     });
 
     infowindow = new google.maps.InfoWindow();
     var service = new google.maps.places.PlacesService(map);
     service.nearbySearch({
-        location: pyrmont,
-        radius: 500,
+        // location: pyrmont,
+        location: firestation,
+        radius: 1609,
         type: ['restaurant']
     }, callback);
 }
@@ -42,3 +44,31 @@ function createMarker(place) {
         infowindow.open(map, this);
     });
 }
+
+//PLAYING THE GAME
+
+var the_number = null;
+function pick_number() {
+    var random_number = Math.floor((Math.random() * 20) + 1);
+    return random_number;
+}
+function make_guess () {
+    var the_guess = $("#guess_input").val();
+    if (the_guess > the_number) {
+        $("#response_div").text("Too High!").css({"background-color": "red", "color": "white"});
+    }
+    else if (the_guess < the_number) {
+        $("#response_div").text("Too Low!").css({"background-color": "blue", "color": "white"});
+    }
+    else if (the_guess === the_number) {
+        $("#response_div").text("You guessed it!").css({"background-color": "green", "color": "white"});
+    }
+    else {
+        $("#response_div").text("Dude, you need to guess a number!").css({"background-color": "purple", "color": "white"});
+    }
+}
+$(document).ready(function () {
+    $(".submit_button").click(function () {
+        make_guess();
+    });
+});
