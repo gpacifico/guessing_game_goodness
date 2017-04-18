1a41910e58b64c738c75c55366a382a902597336
Gwenever Pacifico <gwenever.pacifico@gmail.com>
2017-04-17 20:28:06 -0700

    fixed css and added new font and more divs and changed map radius

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ function Ghostbusters_Map() {
         var firestation = {lat: 40.719560, lng: -74.006626};
         map = new google.maps.Map(document.getElementById('map'), {
             center: firestation,
-            zoom: 14
+            zoom: 18
         });
         var iconBase = 'https://maps.google.com/mapfiles/kml/shapes/';
         var ghost = new google.maps.Marker({
@@ -22,7 +22,7 @@ function Ghostbusters_Map() {
         var service = new google.maps.places.PlacesService(map);
         service.nearbySearch({
             location: firestation,
-            radius: 1609,
+            radius: 161,
             type: ['restaurant']
         }, callback);
     };
@@ -95,6 +95,176 @@ $(document).ready(function () {
         make_guess();
     });
 });
+
+
+
+// //THE OOP VERSION
+//
+// //THE MAP
+//
+// function Ghostbusters_Map() {
+//     this.map = null;
+//     this.infowindow = null;
+//     this.number_of_restaurants = null;
+//     //this.radius = null;
+//     this.initMap = function () {
+//         var firestation = {lat: 40.719560, lng: -74.006626};
+//         map = new google.maps.Map(document.getElementById('map'), {
+//             center: firestation,
+//             zoom: 14
+//         });
+//         var iconBase = 'https://maps.google.com/mapfiles/kml/shapes/';
+//         var ghost = new google.maps.Marker({
+//             position: firestation,
+//             map: map,
+//             icon: iconBase + 'campfire.png'
+//         });
+//         infowindow = new google.maps.InfoWindow();
+//         var service = new google.maps.places.PlacesService(map);
+//         var radi = radius;
+//         service.nearbySearch({
+//             location: firestation,
+//             //radius: 1609,
+//             radius: radi,
+//             type: ['restaurant']
+//         }, callback);
+//     };
+//     this.callback = function(results, status) {
+//         if (status === google.maps.places.PlacesServiceStatus.OK) {
+//             number_of_restaurants = results.length;
+//             console.log(number_of_restaurants);
+//             for (var i = 0; i < results.length; i++) {
+//                 createMarker(results[i]);
+//             }
+//         }
+//     };
+//     this.createMarker = function(place) {
+//         var placeLoc = place.geometry.location;
+//         var marker = new google.maps.Marker({
+//             map: map,
+//             position: place.geometry.location
+//         });
+//         google.maps.event.addListener(marker, 'click', function() {
+//             infowindow.setContent(place.name);
+//             infowindow.open(map, this);
+//         });
+//     }
+//     this.difficulty_level = function() {
+//         var difficulty = $(".difficulty");
+//         var easy_level = $("#easy_radius");
+//         var medium_level = $("#medium_radius");
+//         var hard_level = $("#hard_radius");
+//         if (difficulty && easy_level) {
+//             radi = 1609;
+//             return radi;
+//         }
+//         else if (difficulty && medium_level) {
+//             radi = 3219;
+//             return radi;
+//
+//         }
+//         else if (difficulty && hard_level) {
+//             radi = 4828;
+//             return radi;
+//
+//         }
+//         console.log(radius);
+//     }
+// }
+//
+// //PLAYING THE GAME
+// Ghostbusters_Map();
+//
+// // function Hardness() {
+// //     this.difficulty_level = function() {
+// //         var difficulty = $(".difficulty");
+// //         var easy_level = $("#easy_radius");
+// //         var medium_level = $("#medium_radius");
+// //         var hard_level = $("#hard_radius");
+// //         if (difficulty && easy_level) {
+// //             this.radius = 1609;
+// //         }
+// //         else if (difficulty && medium_level) {
+// //             this.radius = 3219;
+// //         }
+// //         else if (difficulty && hard_level) {
+// //             this.radius = 4828;
+// //         }
+// //         console.log(radius);
+// //     }
+// // }
+//
+//
+// function Guess_Ghosts () {
+//     this.the_number = null;
+//     this.pick_number = function() {
+//         var random_number = Math.floor((Math.random() * number_of_restaurants) + 1);
+//         return random_number;
+//     }
+//     this.reset_input_field = function() {
+//         $("#guess_input").val('');
+//     }
+//     // this.difficulty_level = function() {
+//     //     var difficulty = $(".difficulty");
+//     //     var easy_level = $("#easy_radius");
+//     //     var medium_level = $("#medium_radius");
+//     //     var hard_level = $("#hard_radius");
+//     //     if (difficulty && easy_level) {
+//     //         this.radius = 1609;
+//     //     }
+//     //     else if (difficulty && medium_level) {
+//     //         this.radius = 3219;
+//     //     }
+//     //     else if (difficulty && hard_level) {
+//     //         this.radius = 4828;
+//     //     }
+//     //     console.log(radius);
+//     // }
+//     this.make_guess = function() {
+//         var the_guess = $("#guess_input").val();
+//         if (the_guess > the_number) {
+//             $("#response_div").text("Too High!").css({"background-color": "red", "color": "white"});
+//             this.reset_input_field();
+//         }
+//         else if (!the_guess) {
+//             $("#response_div").text("You need to guess a number").css({"background-color": "green", "color": "white"});
+//             this.reset_input_field();
+//         }
+//         else if (the_guess < 0) {
+//             $("#response_div").text("Ghosts can't haunt negative restaurants!").css({"background-color": "blue", "color": "white"});
+//             this.reset_input_field();
+//         }
+//         else if (the_guess < the_number) {
+//             $("#response_div").text("Too Low!").css({"background-color": "blue", "color": "white"});
+//             this.reset_input_field();
+//         }
+//         else if (the_guess == the_number) {
+//             $("#response_div").text("You guessed it!").css({"background-color": "green", "color": "white"});
+//         }
+//         else {
+//             $("#response_div").text("Dude, you need to guess a number!").css({"background-color": "purple", "color": "white"});
+//             this.reset_input_field();
+//         }
+//     }
+// }
+//
+// Guess_Ghosts();
+//
+// $(document).ready(function () {
+//     //Hardness();
+//     //difficulty_level();
+//     $(".submit_button").click(function () {
+//         make_guess();
+//     });
+// });
+
+
+
+
+
+
+
+
 
 
 
